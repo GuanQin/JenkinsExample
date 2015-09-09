584bc0b8e1efc19debb3df0913d6a3cd4752b631
Qin Guan <qguan@CNqguan.local>
2015-09-09 12:50:13 +0800

    add pwd

--- ios-jenkins-build-script.sh
+++ ios-jenkins-build-script.sh
@@ -8,6 +8,7 @@ DESTINATION="platform=iOS Simulator,name=iPhone 6"
 XCODE_BUILD_COMMAND="xcodebuild -workspace $WORKSPECE_NAME.xcworkspace -scheme $SCHEME_NAME -sdk \"iphoneos\" -configuration Release ONLY_ACTIVE_ARCH=NO TEST_AFTER_BUILD=YES clean build"
 XCODE_TEST_COMMAND="xcodebuild -workspace $WORKSPECE_NAME.xcworkspace -scheme $SCHEME_NAME -destination '$DESTINATION' clean test"
 
+pwd
 cd "$PROJECT_DIR"
 pod install
 eval $XCODE_TEST_COMMAND
