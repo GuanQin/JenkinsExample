a0b9d3657fb94a9c8adef0fc497d095a23934e4a
Qin Guan <qguan@CNqguan.local>
2015-09-09 12:54:18 +0800

    remove useless code

--- ios-jenkins-build-script.sh
+++ ios-jenkins-build-script.sh
@@ -8,8 +8,6 @@ DESTINATION="platform=iOS Simulator,name=iPhone 6"
 XCODE_BUILD_COMMAND="xcodebuild -workspace $WORKSPECE_NAME.xcworkspace -scheme $SCHEME_NAME -sdk \"iphoneos\" -configuration Release ONLY_ACTIVE_ARCH=NO TEST_AFTER_BUILD=YES clean build"
 XCODE_TEST_COMMAND="xcodebuild -workspace $WORKSPECE_NAME.xcworkspace -scheme $SCHEME_NAME -destination '$DESTINATION' clean test"
 
-pwd
-cd "$PROJECT_DIR"
 pod install
 eval $XCODE_TEST_COMMAND
 eval $XCODE_BUILD_COMMAND
